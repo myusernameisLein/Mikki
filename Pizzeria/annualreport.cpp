c67deed841b27969dd09b36b862dd3dc39e6c83b
sqz <vanakaravaj@gmail.com>
2024-09-04 21:29:41 +0300

    Обновлены файлы spendinglist.h и annualreport.cpp

--- Pizzeria/annualreport.cpp
+++ Pizzeria/annualreport.cpp
@@ -1,1 +1,14 @@
+//AnnualReport.cpp
 
+#include "spendinglist.h"
+
+class AnnualReport
+{
+    private:
+        RentRecord* ptrRR; // записи доходов
+        SpendingList* ptrSL; // записи расходов
+        float expenses, rents; // суммы доходов и расходов
+    public:
+        AnnualReport(RentRecord*, SpendingList*);
+        void display(); // отображение годового отчета
+};
